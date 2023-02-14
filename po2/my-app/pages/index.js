2e4d8a5a083d3c85caeafd826c20fa623eb29bef
KaZ12342 <6231501100@lamduan.mfu.ac.th>
2023-02-14 11:50:26 +0700

    sss

--- po2/my-app/pages/index.js
+++ po2/my-app/pages/index.js
@@ -2,9 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import img from "../public/image/1.png"
 import img1 from "../public/image/callbg.jpg"
-import img2 from "../public/image/call.jpg"
+import Header0 from "../public/image/header"
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -40,53 +39,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
+      <Header0/>
+ 
 
       {/* head */}
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <Image src={img2} alt="" width="60" height="34" />
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/pro">productgun</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider" /></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
-              </li>
-            </ul>
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
 
-      <div className='container'>
-
-        <Image src={img1} class="img-fluid" alt="..." />
-
-
-
-      </div>
-
+      
       {/* <div className='container'>
         <div className='row row-cols-2 row-cols-lg-5 g-2 g-lg-3'>
           {card.map((item, index) => (
