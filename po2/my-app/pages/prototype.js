47391e4394b2851c82da4ecbb61b188fa30e4570
male0010 <6231501084@lamduan.mfu.ac.th>
2023-02-15 17:12:20 +0700

    f;b;f.

--- po2/my-app/pages/prototype.js
+++ po2/my-app/pages/prototype.js
@@ -131,7 +131,7 @@ export default function prototype() {
             <div className='row row-cols-2 row-cols-lg-5 g-2 g-lg-3'>
                 {gunlist?.map((item, index) => (
                     <div className="col" >
-                        <div className="card" data-bs-toggle="modal" data-bs-target="#exampleModal" on >
+                        <div className="card" data-bs-toggle="modal" data-bs-target={"#exampleModal"+item.id} on >
                             <Image  width={245} height={200} src={item.path}
                                 className={'imgBack'}  alt="..." />
                             <div className="card-body">
@@ -141,21 +141,23 @@ export default function prototype() {
                             </div>
                         </div>
 
-                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal fade" id={"exampleModal"+item.id} tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                         <div class="modal-dialog">
                                             <div class="modal-content">
                                                 <div class="modal-header">
                                                     <h1 class="modal-title fs-5" id="exampleModalLabel"><Image width={245} height={200} src={item.path}
                                                         className="card-Image-top" alt="..." />
                                                     </h1>
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                    
                                                 </div>
                                                 <div class="modal-body">
-                                                    ...
+                                                {(item.file_name).replace(".jpg",'')}
+                                                </div>
+                                                <div class="modal-body">update on : 
+                                                {item.uploaded_on}
                                                 </div>
                                                 <div class="modal-footer">
                                                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                                    <button type="button" class="btn btn-primary">BUY</button>
                                                 </div>
                                             </div>
                                         </div>
