8a26956c79ae318e595bfd70bc622739bca82252
male0010 <6231501084@lamduan.mfu.ac.th>
2023-03-03 09:15:56 +0700

    assa

--- bpo2/register.php
+++ bpo2/register.php
@@ -1,5 +1,17 @@
-<?php
+<?php require_once "connect.php";
+$acc_username = mysqli_real_escape_string($con,$_POST['acc_username']);
+$acc_password = mysqli_real_escape_string($con,$_POST['acc_password']);
+$acc_name = mysqli_real_escape_string($con,$_POST['acc_name']);
+$acc_lastname = mysqli_real_escape_string($con,$_POST['acc_lastname']);
+$acc_email = mysqli_real_escape_string($con,$_POST['acc_email']);
+$acc_phone = mysqli_real_escape_string($con,$_POST['acc_phone']);
 
-
-
-?>
+$sql = "INSERT INTO account(`acc_username`, `acc_password`, `acc_name`, `acc_lastname`, `acc_email`, `acc_phone`) values ('$acc_username','$acc_password','$acc_name','$acc_lastname','$acc_email','$acc_phone')";
+$result = mysqli_query($con,$sql);
+ if ($result){
+    echo "success";
+ }
+ else {
+    echo "error";
+ }
+ ?>
