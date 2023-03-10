357e6170cf7b21d731a783fd15a2a5c7124b32b5
male0010 <6231501084@lamduan.mfu.ac.th>
2023-03-10 15:15:49 +0700

    sdfsdf

--- po2/my-app/pages/test.js
+++ po2/my-app/pages/test.js
@@ -54,7 +54,117 @@ export default function test() {
                     </div>
                 </div>
 
-                <div className='container text-center my-5 ' style={{ overflowX: 'f' }}>
+
+
+
+
+                <p>
+                    <button class="btn btn-primary"
+                        type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                        GUN
+                    </button>
+                </p>
+                <div class="collapse  " id="collapseExample">
+                    <p className="row" onClick={(event) => {
+                        setup1(event.target.value)
+                    }}>
+                        <button class="btn btn-primary col" value={1} type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample1" aria-expanded="false" aria-controls="multiCollapseExample1">PISTON</button>
+                        <button class="btn btn-primary col" value={2} type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">SMG</button>
+                    </p>
+                    <div class="row">
+                        <div class="col">
+                            <div class="collapse multi-collapse" id="multiCollapseExample1">
+                                {filterdata?.map((item, index) => {
+                                    <div key={item.id_subtype.value} className="">
+                                        <div class="card card-body" key={index}>
+                                            
+                                                <div className='card-body'>
+
+                                                    <h5 className='card-title '>{item.Name_data}</h5>
+                                                    <h6 className='card-subtitle mb-2 text-muted   '>{item.name_subtype}</h6>
+                                                </div>
+                                                <div className='card-body'>
+                                                    <Image width={245} height={200} src={"https://www.imgen.site/imgen2" + item.path}
+                                                        className="card-Image-top img-fluid " alt="" />
+                                                </div>
+                                            </div>
+
+                                        </div>
+
+                                    
+                                })}
+                            </div>
+                        </div>
+                    </div>
+
+
+                    {filterdata?.map((item, index) => {
+                                    <div key={item.id_subtype.value} className="">
+                                        <div class="card card-body" key={index}>
+                                            
+                                                <div className='card-body'>
+
+                                                    <h5 className='card-title '>{item.Name_data}</h5>
+                                                    <h6 className='card-subtitle mb-2 text-muted   '>{item.name_subtype}</h6>
+                                                </div>
+                                                <div className='card-body'>
+                                                    <Image width={245} height={200} src={"https://www.imgen.site/imgen2" + item.path}
+                                                        className="card-Image-top img-fluid " alt="" />
+                                                </div>
+                                            </div>
+
+                                        </div>
+
+                                    
+                                })}
+
+
+
+
+                </div>
+
+
+                {/* <p onClick={(event) => {
+                    setup1(event.target.value)
+                }}>
+                    <button class="btn btn-primary" value={1}
+                        type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                        GUN
+                    </button>
+                </p>
+                <div class="collapse row " id="collapseExample">
+                    {filterdata?.map((item, index) => (
+                       <div key={item.id_subtype.value} className="col">
+                       <div class="card">
+                           <div className="card-body">
+                               <div  className='card-body'>
+
+                                   <h5 className='card-title '>{item.Name_data}</h5>
+                                   <h6 className='card-subtitle mb-2 text-muted   '>{item.name_subtype}</h6>
+                               </div>
+                               <div className='card-body'>
+                                   <Image width={245} height={200} src={"https://www.imgen.site/imgen2" + item.path}
+                                       className="card-Image-top img-fluid " alt="" />
+                               </div>
+                           </div>
+
+                       </div>
+
+                   </div>
+                    ))}
+                </div> */}
+
+                {/* <div key={item.id_subtype.value} className="col">
+                             <div className="card ">
+                                 <div key={index} className="card-body ">
+                                    <h5 className='card-title '>{item.name_subtype}</h5>
+                             </div>
+                             </div>
+                         </div>  */}
+
+
+
+                {/* <div className='container text-center my-5 ' style={{ overflowX: 'f' }}>
                     <div className='row row-cols-2 row-cols-lg-5 g-2 g-lg-3'>
                         {/* {gunlist?.map((item, index) => (
                             <div key={item} className="row" > 
