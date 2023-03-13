ab752c1ebd7f0e1cd2942a8f640010c6e402f48b
male0010 <6231501084@lamduan.mfu.ac.th>
2023-03-13 15:26:55 +0700

    dfds

--- po2/my-app/pages/_app.js
+++ po2/my-app/pages/_app.js
@@ -9,3 +9,4 @@ export default function App({ Component, pageProps }) {
   }, []);
   return <Component {...pageProps} />
 }
+
--- po2/my-app/pages/test2.js
+++ po2/my-app/pages/test2.js
@@ -1,86 +1,42 @@
-import axios from 'axios';
-import React, { useEffect, useState } from'react';
+import Head from 'next/head';
+import { useState } from 'react';
 
-function Test() {
-  const [gunlist, setGunlist] = useState([]);
+export default function Home() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    axios.get('https://www.imgen.site/imgen2/api_male/subtype.php')
-      .then(response => {
-        const data = response.data;
-        const mappedData = data.reduce((result, item) => {
-          const typeId = item.id_type;
-          if (!result[typeId]) {
-            result[typeId] = [];
-          }
-          result[typeId].push(item);
-          return result;
-        }, {});console.log(mappedData);
-        setGunlist(mappedData);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
+  const handleSearch = async () => {
+    const response = await fetch(`https://www.imgen.site/imgen2/api_male/subtype.php?q=${searchTerm}`);
+    const data = await response.json();
+    setUsers(data.items);
+  };
 
-  const type1Array = gunlist[1] || [];
-  const type2Array = gunlist[2] || [];
-  const type3Array = gunlist[3] || [];
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
   return (
-    <>
-      <div className="row">
-        {type1Array.map((item) => (
-          <div key={item.id_data} className="col">
-            <div className="card" data-bs-toggle="modal" data-bs-target={`#exampleModal${item.id_data}`}>
-              <img width={500} height={350} src={`https://www.imgen.site/imgen2${item.path}`} className="card-img-top img-fluid" alt="..." />
-              <div className="card-body itemm">
-                <h5 className="itemm">
-                  {(item.Name_data).toUpperCase()}
-                  <h6 className='card-subtitle mb-2 text-muted'>{item.name_subtype}</h6>
-                </h5>
-              </div>
-            </div>
+    <div>
+      <Head>
+        <title>Github User Search</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Github User Search</h1>
+
+        <div>
+          <input type="text" onChange={handleInputChange} value={searchTerm} />
+          <button onClick={handleSearch}>Search</button>
+        </div>
+
+        {users.map((user) => (
+          <div key={user.id}>
+            <h2>{user.login}</h2>
+            <img src={user.avatar_url} alt={user.login} width={100} />
           </div>
         ))}
-      </div>
-
-      <div className="row">
-        {type2Array.map((item) => (
-          <div key={item.id_data} className="col">
-            <div className="card" data-bs-toggle="modal" data-bs-target={`#exampleModal${item.id_data}`}>
-              <img width={500} height={350} src={`https://www.imgen.site/imgen2${item.path}`} className="card-img-top img-fluid" alt="..." />
-              <div className="card-body itemm">
-                <h5 className="itemm">
-                  {(item.Name_data).toUpperCase()}
-                  <h6 className='card-subtitle mb-2 text-muted'>{item.name_subtype}</h6>
-                </h5>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-      <div className="row">
-        {type3Array.map((item) => (
-          <div key={item.id_data} className="col">
-            <div className="card" data-bs-toggle="modal" data-bs-target={`#exampleModal${item.id_data}`}>
-              <img width={500} height={350} src={`https://www.imgen.site/imgen2${item.path}`} className="card-img-top img-fluid" alt="..." />
-              <div className="card-body itemm">
-                <h5 className="itemm">
-                  {(item.Name_data).toUpperCase()}
-                  <h6 className='card-subtitle mb-2 text-muted'>{item.name_subtype}</h6>
-                </h5>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-
-     
-
-
-        </>
-    );
+      </main>
+    </div>
+  );
 }
-
-export default Test;
