aa251c80df94d4fcf1935014d1c1ca574df8ba45
KaZ12342 <6231501100@lamduan.mfu.ac.th>
2023-03-22 13:34:42 +0700

    dadadadd

--- po2/my-app/pages/_app.js
+++ po2/my-app/pages/_app.js
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import { useEffect } from 'react'
 import '@/styles/fortest.css'
+import '@/styles/index.css'
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
