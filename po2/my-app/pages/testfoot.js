636f1201cc2851c087a8e4651ea9c567a49e6eb6
male0010 <6231501084@lamduan.mfu.ac.th>
2023-05-05 09:28:05 +0700

    ss

--- po2/my-app/pages/testfoot.js
+++ po2/my-app/pages/testfoot.js
@@ -43,13 +43,13 @@ export default function TESTFOOT() {
                         <h3>ITEM</h3></Link>
                     <br></br>
                     <ul className='card-text'>
-                    <Link className='link ' href={"/item"} >
+                    <Link className='link linkk2' href={"/item"} >
                         <li>GUN</li></Link>
                         <br></br>
-                        <Link className='link ' href={"/item"} >
+                        <Link className='link linkk2' href={"/item"} >
                         <li>CHARACTER</li></Link>
                         <br></br>
-                        <Link className='link ' href={"/item"} >
+                        <Link className='link linkk2' href={"/item"} >
                         <li>CAR</li></Link>
                         <br></br>
                     </ul>
